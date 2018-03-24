0ceac43e4142c8126c79d2b13d918e8ea139d852
James Russo <1jlrusso@gmail.com>
2018-03-24 15:36:48 -0700

    new window function to reload to top of page, functionality for top navigation btns, go to top btn functional when window width is 992px or less

--- kinematicsBigFive.js
+++ kinematicsBigFive.js
@@ -1,3 +1,64 @@
+$(window).on('beforeunload', function() {
+   $(window).scrollTop(0);
+});
+
+$(document).ready(function(){
+  $("#conversion-btn").click(function(){
+    $("html, body").animate({
+      scrollTop: $("#conversion-container").offset().top
+    }, "slow")
+  });
+
+  $("#vert-conversion-btn").click(function(){
+    $("html, body").animate({
+      scrollTop: $("#conversion-container").offset().top
+    }, "slow")
+  });
+
+  $("#examples-btn").click(function(){
+    $("html, body").animate({
+      scrollTop: $("#practice-btns-container").offset().top
+    }, "slow")
+  });
+
+  $("#vert-examples-btn").click(function(){
+    $("html, body").animate({
+      scrollTop: $("#practice-btns-container").offset().top
+    }, "slow")
+  });
+
+  $("#go-up-btn").click(function(){
+    $("html, body").animate({
+      scrollTop: 0
+    }, "slow")
+  });
+
+})
+
+window.addEventListener("resize", function(){
+  if(window.innerWidth < 992){
+    $(window).scroll(function(){
+      if($(window).scrollTop() > 600){
+        $("#go-up-container").css("opacity", "1");
+      } else {
+        $("#go-up-container").css("opacity", "0");
+      }
+    })
+  }
+})
+
+window.addEventListener("load", function(){
+  if(window.innerWidth < 992){
+    $(window).scroll(function(){
+      if($(window).scrollTop() > 600){
+        $("#go-up-container").css("opacity", "1");
+      } else {
+        $("#go-up-container").css("opacity", "0");
+      }
+    })
+  }
+})
+
 var horizontalSearchBtn = document.getElementById("horizontal-search-btn"),
 verticalSearchBtn = document.getElementById("vertical-search-btn"),
 searchContainer = document.getElementById("search-container"),
